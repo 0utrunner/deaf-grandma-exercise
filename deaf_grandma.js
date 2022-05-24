383b190fd1df663d413c2c77e7fd37f3467521bc
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-23 22:21:09 -0500

    "Solved unexpected exit after first input"

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,5 +1,28 @@
 function deafGrandma() {
-
+    let input = ''
+    let whisper = /[a-z]/
+    let shout = /[A-Z]/g
+    while(input != "GOODBYE!"){
+      input = prompt("Say something to grandma!")
+      if(input == "GOODBYE!") {
+        input = prompt("LEAVING SO SOON?")
+        if(input == "GOODBYE!"){
+          return alert("LATER, SKATER!")
+        } else if (whisper.test(input) == true){
+          console.log("SPEAK UP, KID!")
+        } else if (shout.test(input) == true){
+          console.log("NO, NOT SINCE 1946!")
+        } else {
+          console.log("WHAT?")
+        }
+      } else if (whisper.test(input) == true){
+        console.log("SPEAK UP, KID!")
+      } else if (shout.test(input) == true){
+        console.log("NO, NOT SINCE 1946!")
+      } else {
+        console.log("WHAT?")
+      }
+    }
 }
-
+//window.prompt
 deafGrandma();
