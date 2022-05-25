9b77e7bd324305968b4f0a690197c88f14e6cad6
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-24 21:44:26 -0500

    Fixed prompt

--- deaf_grandma.js
+++ deaf_grandma.js
@@ -1,28 +1,28 @@
 function deafGrandma() {
-    let input = ''
-    let whisper = /[a-z]/
-    let shout = /[A-Z]/g
-    while(input != "GOODBYE!"){
-      input = prompt("Say something to grandma!")
-      if(input == "GOODBYE!") {
-        input = prompt("LEAVING SO SOON?")
-        if(input == "GOODBYE!"){
-          return alert("LATER, SKATER!")
-        } else if (whisper.test(input) == true){
-          console.log("SPEAK UP, KID!")
-        } else if (shout.test(input) == true){
-          console.log("NO, NOT SINCE 1946!")
-        } else {
-          console.log("WHAT?")
-        }
+  let input = ''
+  let whisper = /[a-z]/
+  let shout = /[A-Z]/g
+  while(input != "GOODBYE!"){
+    input = prompt("Say something to grandma!")
+    if(input == "GOODBYE!") {
+      input = prompt("LEAVING SO SOON?")
+      if(input == "GOODBYE!"){
+        return alert("LATER, SKATER!")
       } else if (whisper.test(input) == true){
-        console.log("SPEAK UP, KID!")
+        prompt("SPEAK UP, KID!")
       } else if (shout.test(input) == true){
-        console.log("NO, NOT SINCE 1946!")
+        prompt("NO, NOT SINCE 1946!")
       } else {
-        console.log("WHAT?")
+        prompt("WHAT?")
       }
+    } else if (whisper.test(input) == true){
+      prompt("SPEAK UP, KID!")
+    } else if (shout.test(input) == true){
+      prompt("NO, NOT SINCE 1946!")
+    } else {
+      prompt("WHAT?")
     }
+  }
 }
 //window.prompt
 deafGrandma();
