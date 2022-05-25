0c59b912e77c653dc9c4a98892f749844cf7b1f3
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-25 16:58:51 -0500

    Python Solved

--- deaf_grandma.py
+++ deaf_grandma.py
@@ -1,5 +1,26 @@
+import re
 def deaf_grandma():
+    leave = False
+    while leave != True:
+        text = input('You say to your grandma...')
+        whisper = re.search("[a-z]", text)
+        if text == 'GOODBYE!':
+            text = input("'LEAVING SO SOON?' she yells.")
+            if text == 'GOODBYE!':
+                print("'LATER, SKATER!' she waves in jest")
+                break
+            elif whisper:
+                print("'SPEAK UP, KID!' she yells. At you.")
+            elif text == '':
+                print("'WHAT?' she yells")
+            else:
+                print("'NO, NOT SINCE 1946!' she yells.")
+        if whisper:
+            print('"SPEAK UP, KID!" she yells.')
+        elif text == '':
+            print('"WHAT?" She yells.')
+        else:
+            print('"NO, NOT SINCE 1946!" she yells.')
 
-
-
+    print('Finally escaped.')
 deaf_grandma()
